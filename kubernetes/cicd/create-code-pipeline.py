41196753f16d339f090a0887545da3a072521854
Jun <jnyanm@amazon.com>
2024-07-12 09:08:38 +0000

    update cicd

--- kubernetes/cicd/create-code-pipeline.py
+++ kubernetes/cicd/create-code-pipeline.py
@@ -1,250 +1,114 @@
 import boto3
-from datetime import datetime
+import os
+import re
+import yaml
+
 # Application Information
 app_name = "flask"
 port = 8080
 namespace = "tenant1"
 repo_prefix = "091063646508.dkr.ecr.us-west-2.amazonaws.com"
-cluster_name = "eks-cluster"
-version = "v1.1.0"
-
+s3_bucket_name = "luffa-codepipeline-poc"
+chart_template_location = f"s3://{s3_bucket_name}/chart-templates/webapplication/"
+chart_app_s3_location = f"s3://{s3_bucket_name}/{namespace}/{app_name}/chart/"
+app_code_location = f"s3://{s3_bucket_name}/{namespace}/{app_name}/code/code.zip"
 # Create CodePipeline client
 session = boto3.Session(profile_name='default', region_name='us-west-2')
 codepipeline = session.client('codepipeline')
 codebuild = session.client('codebuild')
 events_client = boto3.client('events')
-
 serviceRole = "arn:aws:iam::091063646508:role/codepipeline-role"
-s3_bucket_name = "luffa-codepipeline-poc"
-chart_template_location = f"s3://{s3_bucket_name}/chart-templates/webapplication/"
 
-# Get current date and time
-now = datetime.now()
-date_time_str = now.strftime("%Y-%m-%d-%H-%M")
-chart_app_s3_location = f"s3://{s3_bucket_name}/{namespace}/{app_name}/chart/"
-app_code_location = f"s3://{s3_bucket_name}/{namespace}/{app_name}/code/code.zip"
 
-# defined build step
+
+# create codebuild project function
+def create_codebuild_project(project_name, buildspec_file, variables,**kwargs):
+    envs = [{'name': k,'value': str(v),'type': 'PLAINTEXT'} for k,v in variables.items()]
+    with open(buildspec_file) as f:
+        buildspec = f.read()
+    return codebuild.create_project(
+        name=project_name,
+        source={
+            'type': 'CODEPIPELINE',
+            'buildspec': buildspec
+        },
+        artifacts={
+            'type': 'CODEPIPELINE'
+        },
+        environment={
+            'type': 'LINUX_CONTAINER',
+            'computeType': 'BUILD_GENERAL1_SMALL',
+            'image': 'aws/codebuild/amazonlinux2-x86_64-standard:3.0',
+            'privilegedMode': True,
+            'environmentVariables':envs
+        },
+        serviceRole=serviceRole,
+        **kwargs
+    )
+# create codebuild project to build code to docker image,and push to ecr
 def build_step():
-    print("create build project...")
+    print(f"create build project {namespace}-{app_name}-build-project...")
+    envs = {
+        'repo_prefix':repo_prefix,
+        'namespace':namespace,
+        'app_name':app_name
+    }
     codebuild_project_name = f'{namespace}-{app_name}-build-project'
-    rs = codebuild.create_project(
-        name = codebuild_project_name,
-        source = {
-            'type': 'CODEPIPELINE',
-            'buildspec': f"""
-version: 0.2
-phases:
-    install:
-        commands:
-        - echo "Installing dependencies..."
-    pre_build:
-        commands:
-        - echo "Running pre-build commands..."
-    build:
-        commands:
-        - echo "Running build commands..."
-        - aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin {repo_prefix}
-        - docker build -t {repo_prefix}/{app_name}:$version .
-        - docker push {repo_prefix}/{app_name}:$version
-    post_build:
-        commands:
-        - echo "Running post-build commands..."
-        """
-        },
-        artifacts = {
-            'type': 'CODEPIPELINE'
-        },
-        environment = {
-            'type': 'LINUX_CONTAINER',
-            'computeType': 'BUILD_GENERAL1_SMALL',
-            'image': 'aws/codebuild/amazonlinux2-x86_64-standard:3.0',
-            'privilegedMode': True
-        },
-        serviceRole = serviceRole
-        #buildspec = ''
-    )
-    return rs
+    buildspec_file = os.path.join(os.getcwd(), 'buildspec/buildspec_build.yml')
+    return create_codebuild_project(codebuild_project_name, buildspec_file,envs)
 
-# defined deployment step
-def deploy_step():
-    print("create deploy project...")
-    codebuild_project_name = f'{namespace}-{app_name}-deploy-project'
-    rs = codebuild.create_project(
-        name = codebuild_project_name,
-        source = {
-            'type': 'CODEPIPELINE',
-            'buildspec': f"""
-version: 0.2
-phases:
-    install:
-        commands:
-        - echo "Installing helm"
-        - curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-        - helm repo add stable https://charts.helm.sh/stable
-        - curl https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
-        - chmod +x /usr/local/bin/kubectl
-    pre_build:
-        commands:
-        - echo "Update kubeconfig..."
-        - aws eks update-kubeconfig --name {cluster_name}
-        - echo "add helm template..."
-        - mkdir web-application && aws s3 sync {chart_template_location} ./web-application/
-        - echo "replace the template veriables..."
-        - sed -i -e "s/@app-name@/{app_name}/g" -e "s/@image-url@/{repo_prefix}\/{app_name}:$version/g" -e "s/@port@/{port}/g" -e "s/@namespace@/{namespace}/g" ./web-application/values.yaml
-        - echo "upload chart file to s3 ..."
-        - aws s3 sync ./web-application/ {chart_app_s3_location}
-    build:
-        commands:
-        - echo "Running deploy commands..."
-        - helm upgrade --install --force {app_name} web-application 
-    post_build:
-        commands:
-        - echo "Running post-build commands..."
-        """
-        },
-        artifacts = {
-            'type': 'CODEPIPELINE'
-        },
-        environment = {
-            'type': 'LINUX_CONTAINER',
-            'computeType': 'BUILD_GENERAL1_SMALL',
-            'image': 'aws/codebuild/amazonlinux2-x86_64-standard:3.0',
-            'privilegedMode': True
-        },
-        serviceRole = serviceRole
-    )
-    return rs
+# create codebuild project to deploy application to EKS clusters 
+def deploy_to_aws_step(region,cluster_name,codebuild_project_name):
+    print(f"create deploy project {codebuild_project_name}...")
+    envs = {
+        'cluster_name': cluster_name,
+        'app_name': app_name,
+        'repo_prefix': repo_prefix,
+        'port': port,
+        'namespace': namespace,
+        'chart_template_location': chart_template_location,
+        'chart_app_s3_location': chart_app_s3_location,
+        'region':region
+    }
+    buildspec_file = os.path.join(os.getcwd(), 'buildspec/buildspec_deploy.yml')
+    return create_codebuild_project(codebuild_project_name, buildspec_file,envs)
 
-# defined codepipeline step
+# create codepipeline steps for appliactions
 def create_codepipeline():
-    print("create pipeline ...")
-    # Define the pipeline structure
-    pipeline = {
-        'pipeline': {
-            'name': f'{namespace}-{app_name}-pipeline',
-            'roleArn': serviceRole,
-            'artifactStore': {
-                'type': 'S3',
-                'location': s3_bucket_name
-            },
-            'pipelineType': 'V2',
-            'variables': [
-                {
-                    'name': 'version',
-                    'defaultValue': 'v1.0.0',
-                    'description': 'application version'
-                },
-            ],
-            'stages': [
-                {
-                    'name': 'source',
-                    'actions': [
-                        {
-                            'name': 'SourceAction',
-                            'actionTypeId': {
-                                'category': 'Source',
-                                'owner': 'AWS',
-                                'version': '1',
-                                'provider': 'S3'
-                            },
-                            'outputArtifacts': [
-                                {
-                                    'name': 'SourceOutput'
-                                }
-                            ],
-                            'configuration': {
-                                'S3Bucket': s3_bucket_name,
-                                'S3ObjectKey': f'{namespace}/{app_name}/code/code.zip',
-                                'PollForSourceChanges': 'false'
-                            },
-                            'runOrder': 1
-                        }
-                    ]
-                },
-                {
-                    'name': 'build',
-                    'actions': [
-                        {
-                            'name': 'BuildAction',
-                            'actionTypeId': {
-                                'category': 'Build',
-                                'owner': 'AWS',
-                                'version': '1',
-                                'provider': 'CodeBuild'
-                            },
-                            'inputArtifacts': [
-                                {
-                                    'name': 'SourceOutput'
-                                }
-                            ],
-                            'outputArtifacts': [
-                                {
-                                    'name': 'BuildOutput'
-                                }
-                            ],
-                            'configuration': {
-                                "EnvironmentVariables": "[{\"name\":\"version\",\"value\":\"#{variables.version}\",\"type\":\"PLAINTEXT\"}]",
-                                'ProjectName': f'{namespace}-{app_name}-build-project'
-                            },
-                            'runOrder': 1
-                        }
-                    ]
-                },
-                {
-                    'name': 'deploy-to-test',
-                    'actions': [
-                        {
-                            'name': 'BuildAction',
-                            'actionTypeId': {
-                                'category': 'Build',
-                                'owner': 'AWS',
-                                'version': '1',
-                                'provider': 'CodeBuild'
-                            },
-                            'inputArtifacts': [
-                                {
-                                    'name': 'SourceOutput'
-                                }
-                            ],
-                            'outputArtifacts': [
-                                {
-                                    'name': 'DeployTestOutput'
-                                }
-                            ],
-                            'configuration': {
-                                "EnvironmentVariables": "[{\"name\":\"version\",\"value\":\"#{variables.version}\",\"type\":\"PLAINTEXT\"}]",
-                                'ProjectName': f'{namespace}-{app_name}-deploy-project'
-                            },
-                            'runOrder': 1
-                        }
-                    ]
-                }
-            ]
-        }
+    print(f"create code pipeline {namespace}-{app_name}-pipeline...")
+    variables = {
+        'namespace': namespace,
+        'app_name': app_name,
+        'serviceRole': serviceRole,
+        's3_bucket_name': s3_bucket_name
     }
 
-    response = codepipeline.create_pipeline(
-        pipeline=pipeline['pipeline']
-    )
-    return response
+    with open('codepipeline/pipeline.yaml') as f:
+        yaml_content = f.read()
 
+    for var, value in variables.items():
+        yaml_content = re.sub('{'+var+'}', str(value), yaml_content)
+
+    pipeline_config = yaml.safe_load(yaml_content)
+    pipeline = pipeline_config['pipeline']
+    response = codepipeline.create_pipeline(
+        pipeline=pipeline
+    )
+
+# create event rule to auto trigger codepipeline execution after code file upload to s3 bucket
 def create_eventbridge_rule():
     
-    # 创建EventBridge规则
     rule_name = f'{namespace}-{app_name}-s3-trigger'
     rule_response = events_client.put_rule(
         Name=rule_name,
         EventPattern=f'{{"source": ["aws.s3"], "detail-type": ["Object Created"], "detail": {{"bucket": {{"name": ["{s3_bucket_name}"]}}, "object": {{"key": ["{namespace}/{app_name}/code/code.zip"]}}}}}}'
     )
 
-    # 获取CodePipeline ARN
     pipeline_response = codepipeline.get_pipeline(
         name=f'{namespace}-{app_name}-pipeline'
     )
-    pipeline_arn = pipeline_response['metadata']['pipelineArn']
-    
-    # 创建EventBridge目标
+    pipeline_arn = pipeline_response['metadata']['pipelineArn']    
+
     target_response = events_client.put_targets(
         Rule=rule_name,
         Targets=[
@@ -258,22 +122,38 @@ def create_eventbridge_rule():
     
     print(f'EventBridge规则 {rule_name} 已创建,并将在指定的S3对象被上传时触发CodePipeline')
 
-#1. 创建build
-# build_step()
-#2. 创建deploy
-# deploy_step()
-#3. 创建pipeline
-# response=create_codepipeline()
+if __name__ == "__main__":
 
-#4.执行pipeline
-response = codepipeline.start_pipeline_execution(
-    name=f'{namespace}-{app_name}-pipeline',
-    variables=[
-        {
-            'name': 'version',
-            'value': 'v1.0.2'
-        },
+    #1. create codebuild project to build code to docker image,and push it to ecr repo
+    build_step()
+
+    #2. create codebuild project to deploy application to EKS clusters
+    aws_clusters=[
+        {'region':'us-west-2','cluster_name':'eks-cluster','codebuild_project_name':f'{namespace}-{app_name}-us-aws-test'},
+        {'region':'us-east-1','cluster_name':'eks-cluster-us-aws','codebuild_project_name':f'{namespace}-{app_name}-us-aws-prod'},
+        {'region':'eu-central-1','cluster_name':'eks-cluster-eu-aws','codebuild_project_name':f'{namespace}-{app_name}-eu-aws-prod'},
+        {'region':'ap-southeast-1','cluster_name':'eks-cluster-sea-aws','codebuild_project_name':f'{namespace}-{app_name}-sea-aws-prod'}
     ]
-)
-# 5.创建Event Rule规则，自动执行Pipeline
-# create_eventbridge_rule()
+    aliyun_cluster=[]
+    
+    tencent_cluster=[]
+
+    for item in aws_clusters:
+        deploy_to_aws_step(item['region'],item['cluster_name'],item['codebuild_project_name'])
+
+    #3. create codepipeline steps for appliactions
+    response=create_codepipeline()
+
+    #4. execute the pipeline
+    # response = codepipeline.start_pipeline_execution(
+    #     name=f'{namespace}-{app_name}-pipeline',
+    #     variables=[
+    #         {
+    #             'name': 'version',
+    #             'value': 'v1.0.2'
+    #         },
+    #     ]
+    # )
+
+    # 5. create event rule to auto trigger codepipeline execution after code file upload to s3 bucket
+    # create_eventbridge_rule()
